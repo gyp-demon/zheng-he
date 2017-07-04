da3280c263ae8d43d5d79820b8581f837d693dbb
gyp-demon <348146922@qq.com>
2017-07-04 16:47:57 +0800

    first commit

---
+++ xiangmu/es6-compiled.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * Created by ALIENWARE-15 R2 on 2017/7/4.
+ */
+
+var number = [1, 2, 3];
+
+var doubleNumbers = numbers.map(function (number) {
+  return number * 2;
+});
+
+console.log(doubleNumbers);
+
+//# sourceMappingURL=es6-compiled.js.map
---
+++ xiangmu/es6.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ALIENWARE-15 R2 on 2017/7/4.
+ */
+
+
+
+
+
+let number = [1, 2, 3];
+
+let doubleNumbers = numbers.map((number) => number * 2)
+
+console.log(doubleNumbers)
+
+
+let mike = {name: 'mike', age: '40'}
+
+mike = {...mike, sex: 'male'}
+
+console.log(mike);
+
+
+
+
+
+
